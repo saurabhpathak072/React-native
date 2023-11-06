a54252f7c0ffde910d7108f3d4f97a836b315374
Saurabh Pathak <SPathak@eagle.org>
2023-11-06 23:19:51 +0530

    added flexbox to layout

--- App.js
+++ App.js
@@ -3,26 +3,35 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <TextInput placeholder='Your Course goal!' />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your Course goal!' />
         <Button title='Add Goal' />
       </View>
       <View>
-        <Text style={styles.textBox}> List of goals...</Text>
+        <Text> List of goals...</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     padding: 50
   },
   textBox: {
-   
     borderWidth: 2,
     borderColor: "red",
-  
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  textInput:{
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '80%',
+    marginRight:8,
+    padding: 8
+  }
 });
